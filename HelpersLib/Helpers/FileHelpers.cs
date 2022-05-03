3fc4ac76c85ee587b1f35c652d1000a9a500a4d6
Michael Delpach <McoreD@users.noreply.github.com>
2022-05-04 06:35:33 +0800

    Add Zip Manager

---
+++ HelpersLib/Helpers/FileHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace HelpersLibStd
+{
+    public class FileHelpers
+    {
+        public static bool DeleteFile(string filePath, bool sendToRecycleBin = false)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(filePath) && File.Exists(filePath))
+                {
+                    if (sendToRecycleBin)
+                    {
+                        // FileSystem.DeleteFile(filePath, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+                    }
+                    else
+                    {
+                        File.Delete(filePath);
+                    }
+
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                //  DebugHelper.WriteException(e);
+            }
+
+            return false;
+        }
+
+    }
+}
